57da5d92d0939e4326ccae4cec71b78c55402fc9
Tobias Max <tm222tu@student.lnu.se>
2025-09-08 15:35:58 +0200

    Initiated project

---
+++ index.js
@@ -0,0 +1,45 @@
+import { createInterface } from 'node:readline';
+import { stdin, stdout } from 'node:process';
+import chalk from 'chalk'
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+
+const capitalize = (string) => 
+string.charAt(0).toUpperCase() + string.slice(1).toLowerCase()
+
+
+const emptyInput = (input) => {
+  const name = input.trim()
+  return name.length > 0  
+}
+const isValidName = (input) => {
+  const name = input.trim()
+  return /^[\p{L}\s'-]+$/u.test(name)
+}
+
+const askName = () => {
+  rl.question(chalk.green("Hej, vad heter du? \n"), input => {
+    if (!emptyInput(input)) {
+      console.log(chalk.red("Tomt fält, vänligen fyll i ditt name."))
+      return askName()
+    }
+    if (!isValidName(input)){
+      console.log(chalk.red("Ogiltigt tecken, använd endast bokstäver."))
+      return askName()
+    }
+    
+    const normalized = capitalize(input)
+    const reversed   = capitalize(input.split("").reverse().join(""))
+
+    console.log(chalk.magenta(`Hej `) + (chalk.blue(`${normalized}!`)))
+    console.log(chalk.magenta(`Ditt namn baklänges blir `) +(chalk.yellow(`${reversed}`)))
+    rl.close()
+  })
+}
+
+askName()
+ 
+
+
